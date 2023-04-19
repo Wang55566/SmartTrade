97fe0b009a722c3840436f209c27b4d581794cda
Wang55566 <wang6@hawaii.edu>
2023-04-19 12:52:16 -0700

    m

--- migrations/versions/8ebbdbd0ec31_.py
+++ migrations/versions/8ebbdbd0ec31_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 7aadd660555f
+Revision ID: 8ebbdbd0ec31
 Revises: 
-Create Date: 2023-04-19 12:41:40.958916
+Create Date: 2023-04-19 12:51:48.864278
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7aadd660555f'
+revision = '8ebbdbd0ec31'
 down_revision = None
 branch_labels = None
 depends_on = None
