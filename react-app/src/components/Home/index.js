b4ae0ac5fa62323db8c0bfe77696501f5f7f0d91
Wang55566 <wang6@hawaii.edu>
2023-06-18 13:19:39 -0700

    almost

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -18,9 +18,11 @@ function Home(){
         <>
             <div className='home-page'>
                 <div className='money'>
-                    <img src={money} width='400px' height='400px' alt="logo" />
+                    <img src={money} width='600px' height='600px' alt="logo" />
                 </div>
-                <div className="click-me">"Start building your profolio with just a dollor $$"</div>
+                <div className="click-me">"If you don't find a way to make money while you sleep, you will work until you pass away."
+                <div className='home-text'><br></br>- Warren Buffet</div>
+                </div>
             </div>
             <div className='footer'>
                 <div className='footer-text'>
